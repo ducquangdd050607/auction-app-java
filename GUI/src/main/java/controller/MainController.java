dab26cd0fb489e9c2c0b5a6209b2f47049c8d14c
Brother-Cosx <procosx231@gmail.com>
2026-04-03 11:16:46 +0700

    dunno man, vertigo?

--- GUI/src/main/java/controller/MainController.java
+++ GUI/src/main/java/controller/MainController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
