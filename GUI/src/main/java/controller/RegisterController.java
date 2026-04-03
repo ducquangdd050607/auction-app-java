00a2631025f83d274657d1ef1c2d653d9092d242
Brother-Cosx <procosx231@gmail.com>
2026-04-03 15:29:21 +0700

    kiểm chứng mật khẩu, thông tin, redirect về login để đăng nhập lại(?)

--- GUI/src/main/java/controller/RegisterController.java
+++ GUI/src/main/java/controller/RegisterController.java
@@ -50,11 +50,25 @@ public class RegisterController {
 
     @FXML
     void handleConfirm(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
+        if (txtUsername.getText().isEmpty() || txtPassword.getText().isEmpty() ||
+                txtConfirmPassword.getText().isEmpty() ||  txtEmail.getText().isEmpty()) {
+            lblError.setText("Hãy điền đủ thông tin");
+            lblError.setVisible(true);
+        } else if  (!txtConfirmPassword.getText().equals(txtPassword.getText())) {
+            lblError.setText("Mật khẩu không khớp");
+            lblError.setVisible(true);
+        }
+
+        else {
+            isRegister = true;
+            // yêu cầu nhập lại thông tin
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     @FXML
