8c0618fd543dd65659e0b48dfe33823a3a22d710
ducquangdd567 <duongdinhducquang567@gmail.com>
2026-04-05 00:12:54 +0700

    Update basics project & fundamental packages

--- src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
+++ src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class JavaFXLaunch extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXLaunch.class.getResource("views/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXLaunch.class.getResource("views/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
