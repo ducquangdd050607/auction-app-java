b6cf441b4fcd029df4653dbc3db7789061c5404d
Brother-Cosx <procosx231@gmail.com>
2026-04-05 01:21:43 +0700

    re-up

--- src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
+++ src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
@@ -1,19 +1,31 @@
 package com.auctionapp.auctionappjava;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class JavaFXLaunch extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXLaunch.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(JavaFXLaunch.class.getResource("views/MainScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
+        stage.setTitle("Bid88");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/com/auctionapp/auctionappjava/controllers/LoginController.java
+++ src/main/java/com/auctionapp/auctionappjava/controllers/LoginController.java
@@ -1,4 +1,74 @@
 package com.auctionapp.auctionappjava.controllers;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+    static boolean isLoggedIn = false;
+
+    @FXML
+    private Label lblError;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtPrivateKey;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private Button btnAdConfirm;
+
+    @FXML
+    void handleConfirm(ActionEvent event) throws IOException {
+        if (txtUsername.getText().isEmpty() || txtPassword.getText().isEmpty()) {
+            lblError.setText("Hãy điền đủ thông tin");
+            lblError.setVisible(true);
+        } else {
+            isLoggedIn = true;
+            Parent root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/Route.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    @FXML
+    void handleRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/RegisterScreen.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateRegisterStatus(RegisterController.isRegister);
+    }
+
+    @FXML
+    public void updateRegisterStatus(boolean isRegister) {
+        if (isRegister) {
+            lblError.setText("Hãy nhập lại tài khoản.");
+            lblError.setVisible(true);
+        }
+    }
 }
