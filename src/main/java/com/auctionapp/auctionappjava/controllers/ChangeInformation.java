69198ce68a0387d5aec50ac2e9ffb008e9335a89
Brother-Cosx <procosx231@gmail.com>
2026-04-16 20:00:09 +0700

    Change Infor. Confirm

--- src/main/java/com/auctionapp/auctionappjava/controllers/ChangeInformation.java
+++ src/main/java/com/auctionapp/auctionappjava/controllers/ChangeInformation.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
@@ -15,24 +17,11 @@ import java.io.IOException;
 
 public class ChangeInformation {
 
-    private Stage stage;
-    private Parent root;
-    private Scene scene;
-
     @FXML
     private Label lblError;
 
     @FXML
-    private Label lblExtraInfo;
-
-    @FXML
-    private TextField txtBirthday;
-
-    @FXML
     private TextField txtEmail;
-
-    @FXML
-    private TextField txtName;
 
     @FXML
     private TextField txtUsername;
@@ -41,30 +30,41 @@ public class ChangeInformation {
     void handleConfirm(ActionEvent event) throws IOException {
         // TODO: Thay đổi thông tin trong database
 
-        if (txtEmail.getText().trim().isEmpty() || txtName.getText().trim().isEmpty() || txtUsername.getText().trim().isEmpty() || txtBirthday.getText().trim().isEmpty()) {
+        if (txtEmail.getText().trim().isEmpty() || txtUsername.getText().trim().isEmpty()) {
             lblError.setText("Hãy điền tất cả thông tin");
             lblError.setVisible(true);
             lblError.setTextFill(Color.web("#FF8A80"));
         } else {
-            root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/WalletScreen.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.sizeToScene();
-            stage.centerOnScreen();
-            stage.show();
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Chắc chưa?");
+            alert.setHeaderText("Bạn có muốn đổi thông tin không?");
+
+            alert.showAndWait().ifPresent(response -> {
+
+                if (response == ButtonType.OK) {
+
+                    alert.close();
+
+                    Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+                    successAlert.setTitle("Thông báo");
+                    successAlert.setHeaderText(null);
+                    successAlert.setContentText("Đã thay đổi thông tin thành công!");
+                    successAlert.showAndWait();
+
+                    Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    currentStage.close();
+                } else {
+                    alert.close();
+
+                }
+            });
         }
     }
 
     @FXML
     void handleBack(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/WalletScreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.show();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
     }
-
 }
