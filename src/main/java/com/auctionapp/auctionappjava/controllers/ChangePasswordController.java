195b780248915cd9a986bb9561059f86785151f8
Brother-Cosx <procosx231@gmail.com>
2026-04-16 19:59:54 +0700

    Change Password Confirm

--- src/main/java/com/auctionapp/auctionappjava/controllers/ChangePasswordController.java
+++ src/main/java/com/auctionapp/auctionappjava/controllers/ChangePasswordController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.paint.Color;
@@ -14,10 +16,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ChangePasswordController {
-
-    private Stage stage;
-    private Parent root;
-    private Scene scene;
 
     @FXML
     private Label lblMessage;
@@ -47,9 +45,28 @@ public class ChangePasswordController {
             lblMessage.setTextFill(Color.web("#FF8A80"));
         }
         else {
-            // Xong việc thì tự động đóng cửa sổ modal này lại
-            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            currentStage.close();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Chắc chưa?");
+            alert.setHeaderText("Bạn có muốn đổi mật khẩu không?");
+
+            alert.showAndWait().ifPresent(response -> {
+
+                if (response == ButtonType.OK) {
+
+                    alert.close();
+                    Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+                    successAlert.setTitle("Thông báo");
+                    successAlert.setHeaderText(null);
+                    successAlert.setContentText("Đã thay đổi mật khẩu thành công!");
+                    successAlert.showAndWait();
+
+                    Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    currentStage.close();
+
+                } else {
+                    alert.close();
+                }
+            });
         }
     }
 }
