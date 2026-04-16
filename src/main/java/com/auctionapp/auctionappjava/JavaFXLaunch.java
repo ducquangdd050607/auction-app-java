7e57cee8a87716b8379d7e3da7db58ebe85f2a68
Brother-Cosx <procosx231@gmail.com>
2026-04-16 19:59:30 +0700

    Turn-off Confirm

--- src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
+++ src/main/java/com/auctionapp/auctionappjava/JavaFXLaunch.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -24,7 +26,27 @@ public class JavaFXLaunch extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
+
+        // thông báo tắt bằng "X"
+        stage.setOnCloseRequest(event -> {
+            // Ngăn chặn việc đóng cửa sổ ngay lập tức để chờ xác nhận
+            event.consume();
+            handleExit(stage);
+        });
     }
+
+    private void handleExit(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Xác nhận thoát");
+        alert.setHeaderText("Bạn có chắc chắn muốn thoát ứng dụng không?");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                stage.close();
+            }
+        });
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
