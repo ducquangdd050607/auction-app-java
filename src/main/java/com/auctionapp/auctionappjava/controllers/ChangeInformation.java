bf1ba342656b18558ca4172624ec4aeeff237ee0
Brother-Cosx <procosx231@gmail.com>
2026-04-18 20:25:51 +0700

    Remove wastes

--- src/main/java/com/auctionapp/auctionappjava/controllers/ChangeInformation.java
+++ src/main/java/com/auctionapp/auctionappjava/controllers/ChangeInformation.java
@@ -2,7 +2,6 @@ package com.auctionapp.auctionappjava.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
