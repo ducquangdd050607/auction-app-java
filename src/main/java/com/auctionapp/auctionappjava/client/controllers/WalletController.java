3d09f5c754c4cee584db797d597db1755cad75fd
Brother-Cosx <procosx231@gmail.com>
2026-04-19 18:34:51 +0700

    Sáp nhập ChangingPassword, Wallet vào Account

--- src/main/java/com/auctionapp/auctionappjava/client/controllers/WalletController.java
+++ src/main/java/com/auctionapp/auctionappjava/client/controllers/WalletController.java
@@ -11,18 +11,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 public class WalletController {
 
-    private Stage stage;
-    private Parent root;
-    private Scene scene;
-
     @FXML
     private Label lblBalance;
 
     @FXML
     private Label lblEmail;
-
-    @FXML
-    private Label lblFullName;
 
     @FXML
     private Label lblRole;
@@ -32,25 +25,37 @@ public class WalletController {
 
     @FXML
     void handleChangingInformation(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/ChangeInformation.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.show();
+        Parent modalRoot = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/ChangeInformation.fxml"));
+        Stage modalStage = new Stage();
+        modalStage.setTitle("Đổi mật khẩu");
+
+        Stage parentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        modalStage.initModality(javafx.stage.Modality.WINDOW_MODAL);
+        modalStage.initOwner(parentStage);
+
+        Scene modalScene = new Scene(modalRoot);
+        modalStage.setScene(modalScene);
+        modalStage.setResizable(false);
+
+        modalStage.showAndWait();
 
     }
 
     @FXML
     void handleDeposit(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/DepositScreen.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.show();
+        Parent modalRoot = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/DepositScreen.fxml"));
+        Stage modalStage = new Stage();
+        modalStage.setTitle("Đổi mật khẩu");
+
+        Stage parentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        modalStage.initModality(javafx.stage.Modality.WINDOW_MODAL);
+        modalStage.initOwner(parentStage);
+
+        Scene modalScene = new Scene(modalRoot);
+        modalStage.setScene(modalScene);
+        modalStage.setResizable(false);
+
+        modalStage.showAndWait();
     }
     
     @FXML
