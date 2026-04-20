036615b76176fb092708b5eda4c4c8a98d7c7d99
Le Vu Binh <fallonmagnus14032007@gmail.com>
2026-04-20 15:52:40 +0700

    thêm mysql.connector.j' vào module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@ module com.auctionapp.auctionappjava {
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
+    requires mysql.connector.j;
 
     opens com.auctionapp.auctionappjava.client.controllers to javafx.fxml;
     exports com.auctionapp.auctionappjava;
