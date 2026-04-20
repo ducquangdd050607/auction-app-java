7c74eef688c769261b0b92eb66df255cf7eb0fb6
Brother-Cosx <procosx231@gmail.com>
2026-04-20 22:20:51 +0700

    (chả hiểu sao mất) Refine Confirm Bidding

--- src/main/java/com/auctionapp/auctionappjava/client/controllers/ConfirmBiddingController.java
+++ src/main/java/com/auctionapp/auctionappjava/client/controllers/ConfirmBiddingController.java
@@ -6,31 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class ConfirmBiddingController {
 
+    private long defaultAmount = 0;
     private Stage stage;
-    private Parent root;
     private Scene scene;
-
-    @FXML
-    private Button Plus100;
-
-    @FXML
-    private Button Plus1000;
-
-    @FXML
-    private Button Plus10000;
-
-    @FXML
-    private Button Plus100000;
 
     @FXML
     private Label lblBalance;
@@ -39,14 +28,60 @@ public class ConfirmBiddingController {
     private Label lblError;
 
     @FXML
-    private Label lblRole;
-
-    @FXML
     private TextField txtSetPrice;
 
     @FXML
+    private Button btnMore;
+
+    @FXML
+    public void initialize() {
+        btnMore.setManaged(false);
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        formatter.applyPattern("#,###");
+
+        txtSetPrice.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (newValue == null || newValue.isEmpty()) {
+                return;
+            }
+
+            String cleanInput = newValue.replaceAll("[^\\d]", "");
+
+            try {
+                if (cleanInput.isEmpty()) {
+                    txtSetPrice.setText("");
+                    return;
+                }
+
+                long amount = Long.parseLong(cleanInput);
+                String formattedStr = formatter.format(amount);
+
+                formattedStr = formattedStr.replaceAll(",", ".");
+
+                if (!newValue.equals(formattedStr)) {
+
+                    int cursorPosition = txtSetPrice.getCaretPosition();
+                    int lengthDiff = formattedStr.length() - newValue.length();
+
+                    txtSetPrice.setText(formattedStr);
+
+                    txtSetPrice.positionCaret(cursorPosition + lengthDiff);
+                }
+
+            } catch (NumberFormatException e) {
+                // TODO: xử lí exception số quá lớn
+
+                txtSetPrice.setText(oldValue);
+            }
+        });
+    }
+
+
+
+
+    @FXML
     void handleBack(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/InsideItemScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/AuctionDetailScreen.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -54,8 +89,21 @@ public class ConfirmBiddingController {
     }
 
     @FXML
-    void handlePlus100(ActionEvent event) {
+    void handlePreset(ActionEvent event) {
 
+        Button clickedButton = (Button) event.getSource();
+        String presetValue = clickedButton.getText();
+
+        long amount = purifyingText(presetValue);
+
+        if (txtSetPrice.getText().isEmpty()) {
+            txtSetPrice.setText(String.valueOf(amount));
+            defaultAmount = amount;
+
+        } else {
+            defaultAmount += amount;
+            txtSetPrice.setText(String.valueOf(defaultAmount));
+        }
     }
 
 
@@ -64,8 +112,63 @@ public class ConfirmBiddingController {
         if (txtSetPrice.getText().isEmpty()) {
             lblError.setText("Hãy nhập giá tiền cược.");
             lblError.setTextFill(Color.web("#FF8A80"));
+            btnMore.setManaged(false);
+            btnMore.setVisible(false);
         }
 
+        else if (purifyingText(lblBalance.getText()) < defaultAmount) {
+            lblError.setText("Không đủ tiền trong số dư!");
+            lblError.setTextFill(Color.web("#FF8A80"));
+            btnMore.setManaged(true);
+            btnMore.setVisible(true);
+        }
+
+        else {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Chắc chưa?");
+            alert.setHeaderText("Bạn CHẮC muốn ĐặC KưỢk không?");
+
+            alert.showAndWait().ifPresent(response -> {
+
+                if (response == ButtonType.OK) {
+
+                    alert.close();
+                    Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+                    successAlert.setTitle("Thông báo");
+                    successAlert.setHeaderText(null);
+                    successAlert.setContentText("Đã ĐặC KưỢk xong!");
+                    successAlert.showAndWait();
+
+                    Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    currentStage.close();
+
+                    //Reload lại list
+
+                } else {
+                    alert.close();
+                }
+            });
+        }
     }
 
+    @FXML
+    void handleWallet(ActionEvent event) throws IOException {
+        Parent modalRoot = FXMLLoader.load(getClass().getResource("/com/auctionapp/auctionappjava/views/DepositScreen.fxml"));
+        Stage modalStage = new Stage();
+        modalStage.setTitle("moneymoneymoney");
+
+        Stage parentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        modalStage.initModality(javafx.stage.Modality.WINDOW_MODAL);
+        modalStage.initOwner(parentStage);
+
+        Scene modalScene = new Scene(modalRoot);
+        modalStage.setScene(modalScene);
+        modalStage.setResizable(false);
+
+        modalStage.showAndWait();
+    }
+    long purifyingText(String price) {
+        return Long.parseLong(price.replaceAll("[^\\d]", ""));
+    }
 }
