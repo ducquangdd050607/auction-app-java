174944362948acbf852eff5c1c48704fd4916b56
Le Vu Binh <fallonmagnus14032007@gmail.com>
2026-04-20 13:00:54 +0700

    thêm application.properties lưu url của database

--- src/main/java/com/auctionapp/auctionappjava/Launcher.java
+++ src/main/java/com/auctionapp/auctionappjava/Launcher.java
@@ -2,7 +2,7 @@ package com.auctionapp.auctionappjava;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class   Launcher {
     public static void main(String[] args) {
         Application.launch(JavaFXLaunch.class, args);
     }
