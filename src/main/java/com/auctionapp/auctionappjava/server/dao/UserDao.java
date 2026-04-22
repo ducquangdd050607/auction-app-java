4b2487b8a0604470bf54b2bd2945e45f028fb402
Le Vu Binh <fallonmagnus14032007@gmail.com>
2026-04-22 23:06:48 +0700

    them

--- src/main/java/com/auctionapp/auctionappjava/server/dao/UserDao.java
+++ src/main/java/com/auctionapp/auctionappjava/server/dao/UserDao.java
@@ -1,4 +1,32 @@
 package com.auctionapp.auctionappjava.server.dao;
 
+import com.auctionapp.auctionappjava.common.enums.Role;
+import com.auctionapp.auctionappjava.common.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
 public interface UserDao {
+
+    // --- Nhóm chức năng Thêm mới & Truy vấn ---
+
+    User save(User u);
+
+    Optional<User> findById(UUID id);
+
+    Optional<User> findByUsername(String username);
+
+    List<User> findAll();
+
+    long countAll();
+
+    // --- Nhóm chức năng Cập nhật thông tin ---
+
+    void updateRole(UUID id, Role role);
+
+    void updateProfile(UUID id, String fullName, String email);
+
+    void updatePassword(UUID id, String hash, String salt);
 }
