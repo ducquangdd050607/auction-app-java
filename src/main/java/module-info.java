73cd9dde67393f6c0fc9eeff073d19a57decb62f
Brother-Cosx <procosx231@gmail.com>
2026-04-26 14:31:09 +0700

    fix module-info.java, again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,13 @@
 module com.auctionapp.auctionappjava {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires java.sql;
 
     opens com.auctionapp.auctionappjava.client.controllers to javafx.fxml;
+    opens com.auctionapp.auctionappjava.common.util to javafx.fxml;
 
     exports com.auctionapp.auctionappjava.client;
     exports com.auctionapp.auctionappjava.server;
     exports com.auctionapp.auctionappjava.common.model;
+
 }
