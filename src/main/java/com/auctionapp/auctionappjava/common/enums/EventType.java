09b50059ce0dac4a691a8c0d65d604f81222eb98
Brother-Cosx <procosx231@gmail.com>
2026-05-28 01:23:54 +0700

    Removed unused enums and TimeUtil

--- src/main/java/com/auctionapp/auctionappjava/common/enums/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.auctionapp.auctionappjava.common.enums;
-
-public enum EventType {
-  AUCTION_UPDATED, // Thông tin auction thay đổi (title, price, time…)
-  BID_PLACED, // Có người đặt giá
-  AUCTION_STATUS_CHANGED, // Trạng thái auction đổi (OPEN → RUNNING → FINISHED)
-  SYSTEM_MESSAGE // Thông báo hệ thống (log, cảnh báo…) (vì ko có method ở dưới nên k cần ;)
-}
-// các event xảy ra trong đấu giá
